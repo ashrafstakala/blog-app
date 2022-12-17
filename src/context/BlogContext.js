c6af75629487ebf2afbae9113de937cf23ab7b2f
ashrafstakala <ashrafstakala@gmail.com>
2022-12-17 12:19:12 +0200

    add state in context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+  const [blogBosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #${blogPosts.length + 1}` },
+    ]);
+  };
+
   const blogPosts = [{ title: 'Blog Post #1' }, { title: 'Blog Post #2' }];
 
   return (
-    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
   );
 };
 
